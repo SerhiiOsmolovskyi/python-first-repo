396bddd9544761e2a505bd1a1245a48fdef79115
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-11-17 13:45:26 -0700

    Lesson 2

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Hello world!")
-print ("First commit")
-print ("Hello Git")
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+
+cat ={
+    "nick": "Simon",
+    "age": 7,
+    "characteristics": ["sweet", "biting"]
+}
+
+age = cat.get("age")
+cat.update(info)
+
+print("Updated cat dictionary:", cat)
